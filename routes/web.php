064f927ba9199cacf5bc483a53747aad5e052f98
hitech-inc <hitech@mail.kz>
2018-04-05 00:52:40 +0600

    Auth routes fixed

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,24 @@ Route::get('/logout', function(Request $request) {
 	$request->session()->invalidate();
 	return redirect('/login');
 });
-Auth::routes();
 
+// Auth
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+// Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+// Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+// backend
 Route::prefix('backend')->as('backend.')->middleware(['auth', 'role:administrator'])->group(function() {
 	Route::get('/', 'BackendController@index')->name('index');
 
